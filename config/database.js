63f70666c08506fcc04e55e8109b1132974ac5a7
rahul <104730466+rahulr0@users.noreply.github.com>
2022-11-10 08:38:56 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,15 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': "localhost",
+        'user': "root",//root
+        'password': ""
+    },
+    'database': "sample"
+    // 'connection': {
+    //     'host': process.env.DB_HOST,
+    //     'user': process.env.DB_USER,
+    //     'password': process.env.DB_PASS
+    // },
+    // 'database': process.env.DB_NAME
+};
